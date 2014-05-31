e6c3fa67cd0e098a943ee101b0bd1c7f011acfe6
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-05-31 12:57:40 +0200

    Created a specific tabListener

--- src/com/jiahaoliuliu/actionbartabs/MainActivity.java
+++ src/com/jiahaoliuliu/actionbartabs/MainActivity.java
@@ -1,59 +1,47 @@
 package com.jiahaoliuliu.actionbartabs;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends SherlockFragmentActivity {
+	  // store the active tab here
+	  public static String ACTIVE_TAB = "activeTab";
 
-	// store the active tab here
-	public static String ACTIVE_TAB = "activeTab";
-	private ActionBar actionBar;
-	
-	private String[] tabs = {"Tab 1", "Tab 2"};
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		actionBar = getSupportActionBar();
-		for (String tabTitle : tabs) {
-			ActionBar.Tab tab = actionBar.newTab().setText(tabTitle)
-					.setTabListener(tabListener);
-			actionBar.addTab(tab);
-		}
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-	}
-	
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-	  // save active tab
-	  outState.putInt(ACTIVE_TAB,
-	          getSupportActionBar().getSelectedNavigationIndex());
-	  super.onSaveInstanceState(outState);
-	}
-	
-	private ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-		@Override
-		public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-		    // When the given tab is selected, show the tab contents in the
-		    // container view.
-		    Fragment fragment = new WhiteFragment();
-			if (tab.getText().equals("Tab 2")) {
-				fragment = new BlackFragment();
-			}
-		    getSupportFragmentManager().beginTransaction()
-		        .replace(R.id.content_frame, fragment).commit();		}
+	    final ActionBar actionBar = getSupportActionBar();
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	    // add the first tab which is an instance of TabFragment1
+	    Tab tab1 = actionBar.newTab()
+	              .setText("TabTitle 1")
+	              .setTabListener(new TabListener<WhiteFragment>(
+	               this, "tab1", WhiteFragment.class));
+	    actionBar.addTab(tab1);
 
-		@Override
-		public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-		}
+	    // add the second tab which is an instance of TabFragment2
+	    Tab tab2 = actionBar.newTab()
+	           .setText("TabTitle 2")
+	           .setTabListener(new TabListener<BlackFragment>(
+	                this, "tab2", BlackFragment.class));
+	    actionBar.addTab(tab2);
 
-		@Override
-		public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-		}
-	};
+	    // check if there is a saved state to select active tab
+	    if( savedInstanceState != null ){
+	      getSupportActionBar().setSelectedNavigationItem(
+	                  savedInstanceState.getInt(ACTIVE_TAB));
+	    }
+	  }
+
+	  @Override
+	  protected void onSaveInstanceState(Bundle outState) {
+	    // save active tab
+	    outState.putInt(ACTIVE_TAB,
+	            getSupportActionBar().getSelectedNavigationIndex());
+	    super.onSaveInstanceState(outState);
+	  }
 }
